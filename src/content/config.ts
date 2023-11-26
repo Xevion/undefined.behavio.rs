2184dcd202bb3d4fb7903f87f1bd86ecddb2b1db
Xevion <xevion@xevion.dev>
2023-11-26 07:52:39 -0600

    Use date-fns for parsing pubDate, use Date instead of string

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,16 @@
 import { z, defineCollection } from 'astro:content';
+import { parse } from 'date-fns';
 
 const blogCollection = defineCollection({
   type: 'content', // v2.5.0 and later
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    pubDate: z.string(),
+    pubDate: z.string().transform((date) => 
+      z.date().parse(
+        parse(date, 'yyyy-MM-dd HH:mm:ss XXXX', new Date())
+      )
+    ),
     tags: z.array(z.string()),
     preview_image: z.string().optional(),
   }),
