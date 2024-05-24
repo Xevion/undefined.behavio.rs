e1850d5279c2db4453a0a0c1159ca7fbf645d8b9
Xevion <xevion@xevion.dev>
2024-05-24 16:42:51 -0500

    ImportMetaEnv interface definition

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly PUBLIC_VERCEL_GIT_REPO_OWNER: string;
+    readonly PUBLIC_VERCEL_GIT_REPO_SLUG: string;
+    readonly PUBLIC_VERCEL_GIT_COMMIT_SHA: string;
+}
+  
+interface ImportMeta {
+readonly env: ImportMetaEnv;
+}
