5b7cbfe869704ec8a33128b065dad91df9fbb42e
Xevion <xevion@xevion.dev>
2024-05-24 01:32:31 -0500

    Display posts & filter tag list page

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,5 +1,11 @@
 import { type CollectionEntry } from "astro:content";
 
-export function shouldShowPost(post: CollectionEntry<"blog">) {
+type Predicate<T> = (value: T) => boolean;
+
+export function shouldShowPost(post: CollectionEntry<"blog">): boolean {
     return import.meta.env.DEV || !post.data.draft;
+}
+
+export function hasTag(tag: string): Predicate<CollectionEntry<"blog">> {
+    return (post) => post.data.tags.includes(tag)
 }
